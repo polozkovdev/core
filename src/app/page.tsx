1bc61d614e3334532c1c150befbbfeee9651e75b
rpalazkou-ms <r.palazkou@moonactive.com>
2024-03-02 00:32:23 +0300

    [feat]: prisma, docker

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
 import { Button } from "@/shared/ui/button";
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient();
 
 export default function Home() {
+  const courses = await client.course.findMany();
+  console.log("courses", courses);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Button>Button</Button>
